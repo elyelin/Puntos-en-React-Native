7f7bbe124d1bc7df5245aac44a33f4f3d7a3681f
Elyelin Carrasquero <elyelin15@gmail.com>
2020-06-22 16:20:11 -0300

    primer comit

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Map, Modal, Panel, Input } from "./components";
 
 export default function App() {
+  const [puntos, setPuntos] = useState([]);
+  const [puntoTemp, setPuntoTemp] = useState({});
+  const [nombre, setNombre] = useState("");
+  const [visibility, setVisibility] = useState(false);
+
+  const handleLongPress = ({ nativeEvent }) => {
+    setPuntoTemp(nativeEvent.coordinate);
+    setVisibility(true);
+  };
+
+  const handleChangeText = (text) => {
+    setNombre(text);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Map onLongPress={handleLongPress} />
+      <Panel />
+      <Modal visibility={true}>
+        <Input
+          title="Nombre"
+          placeholder="Nombre del punto"
+          onChangeText={handleChangeText}
+        />
+      </Modal>
     </View>
   );
 }
@@ -12,8 +35,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
   },
 });
---
+++ components/Map.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MapView from "react-native-maps";
+import { StyleSheet, Dimensions } from "react-native";
+
+export default ({ onLongPress }) => {
+  return <MapView style={styles.map} onLongPress={onLongPress} />;
+};
+
+const styles = StyleSheet.create({
+  map: {
+    height: Dimensions.get("window").height - 80,
+    width: Dimensions.get("window").width,
+  },
+});
